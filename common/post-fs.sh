ec9e60b1a2bf69ede36384ae398a99bf672d032b
Shwise <Shwise@users.noreply.github.com>
2016-10-10 20:51:41 -0400

    Update post-fs.sh

--- common/post-fs.sh
+++ common/post-fs.sh
@@ -1,4 +1,33 @@
 #!/system/bin/sh
+ro.build.id=NDE63H
+ro.build.display.id=NDE63H
+ro.build.version.incremental=eng.kamma.20161005.160756
+ro.build.version.sdk=24
+ro.build.version.preview_sdk=0
+ro.build.version.codename=REL
+ro.build.version.all_codenames=REL
+ro.build.version.release=7.1
+ro.build.version.security_patch=2016-10-05
+ro.build.version.base_os=
+ro.build.date=Thu Sep  8 18:55:20 UTC 2016
+ro.build.date.utc=1473360920
+ro.build.type=user-debug
+ro.build.user=android-build
+ro.build.host=wpdt2.hot.corp.google.com
+ro.build.tags=release-keys
+ro.build.flavor=marlin-user
+ro.product.model=Pixel XL
+ro.product.brand=google
+ro.product.name=marlin
+ro.product.device=marlin
+ro.product.board=marlin
 
+ro.product.manufacturer=Google
+
+ro.build.description=marlin-user 7.1 NDE63H 3256426 release-keys
+ro.build.fingerprint=google/marlin/marlin:7.1/NDE63H/3256426:user/release-keys
+
+ro.setupwizard.enterprise_mode=1
+ro.opa.eligible_device=true
 # This script will be executed in post-fs mode
 # More info in the main Magisk thread
